63e6b74bee166abb8a88077ff02c5d5814fa1b49
Niraj Kumar <nirajk23523@gmail.com>
2023-05-21 17:14:27 +0530

    returned req from auth and added assets

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,6 +4,44 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 // to give web token for authorisation
 // authentication -> verifying the identity of a user or entity
-// authorization -> t determines what a user is allowed to do after their identity has been authenticated
+// authorization -> it determines what a user is allowed to do after their identity has been authenticated
 
 import User from "../models/User.js";
+
+export const register = async (req, res) => {
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation,
+        } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Math.floor(Math.random() * 1000),
+            impressions: Math.floor(Math.random() * 1000),
+            // we are using Math.random() to generate random numbers
+            // for the above two fields because we are
+            // not implementing thier functionality in this project as of now
+        });
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
