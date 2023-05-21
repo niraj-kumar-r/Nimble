a1744208d4d4b56ce491a7104cb48a82ff2ab8ce
Niraj Kumar <nirajk23523@gmail.com>
2023-05-21 16:47:16 +0530

    made User model from schema

---
+++ server/controllers/auth.js
@@ -0,0 +1,9 @@
+import bcrypt from "bcrypt";
+// to encrypt passwords
+
+import jwt from "jsonwebtoken";
+// to give web token for authorisation
+// authentication -> verifying the identity of a user or entity
+// authorization -> t determines what a user is allowed to do after their identity has been authenticated
+
+import User from "../models/User.js";
