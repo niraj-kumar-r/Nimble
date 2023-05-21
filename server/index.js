a1f99de041938112d98468d15193f190f1bbddfa
Niraj Kumar <nirajk23523@gmail.com>
2023-05-21 15:28:27 +0530

    configuration

--- server/index.js
+++ server/index.js
@@ -8,3 +8,21 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+
+/* CONFIGURATIONS */
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// above two lines are only required for ES6 modules i.e. when we use
+// types : "module" in package.json and use import instead of require
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
