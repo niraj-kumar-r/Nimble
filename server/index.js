9ec844b346d6f33dcdb9a7a72f7be7b374063f22
Niraj Kumar <nirajk23523@gmail.com>
2023-05-22 13:46:40 +0530

    added the posts route

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATIONS */
 
@@ -46,9 +49,12 @@ const upload = multer({ storage: storage });
 app.post("auth/register", upload.single("picture"), register);
 // this is separate from authRoutes because we need to upload the file
 
+app.post("/posts", verifyToken, upload.single("picture", createPost));
+
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
