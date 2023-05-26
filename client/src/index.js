067014a6ddfc6cfad89e53c6d85f43fcc39eb820
Niraj Kumar <nirajk23523@gmail.com>
2023-05-26 22:03:13 +0530

    imported redux stuff

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// redux configuration
+import authReducer from "./state";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+// below import specifically for redux-persist node module
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
