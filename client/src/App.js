a75a4e3eb50460943ce8ba7c50e8f75785da2102
Niraj Kumar <nirajk23523@gmail.com>
2023-05-27 16:21:38 +0530

    used material ui theme

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,32 @@ import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
 // this is why we added include "src" in jsconfig
 
+// for theme
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme";
+
 function App() {
+    const mode = useSelector((state) => state.mode);
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
     return (
         <div className="app">
             <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<LoginPage />} />
-                    <Route path="/home" element={<HomePage />} />
-                    <Route path="/profile/:userId" element={<ProfilePage />} />
-                </Routes>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    {/* resets the css to very basic defined in material ui */}
+                    <Routes>
+                        <Route path="/" element={<LoginPage />} />
+                        <Route path="/home" element={<HomePage />} />
+                        <Route
+                            path="/profile/:userId"
+                            element={<ProfilePage />}
+                        />
+                    </Routes>
+                </ThemeProvider>
             </BrowserRouter>
         </div>
     );
