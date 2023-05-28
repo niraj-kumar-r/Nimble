59f818b3ef297bf88eff653dddc71c4339992da0
Niraj Kumar <nirajk23523@gmail.com>
2023-05-28 19:37:15 +0530

    desktop nav

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -72,6 +72,56 @@ const Navbar = () => {
                     </FlexBetween>
                 )}
             </FlexBetween>
+
+            {/* Desktop Nav */}
+            {isNonMobileScreens ? (
+                <FlexBetween gap="2rem">
+                    <IconButton onClick={() => dispatch(setMode())}>
+                        {theme.palette.mode === "dark" ? (
+                            <DarkMode sx={{ fontSize: "25px" }} />
+                        ) : (
+                            <LightMode sx={{ color: dark, fontSize: "25px" }} />
+                        )}
+                    </IconButton>
+                    <Message sx={{ fontSize: "25px" }} />
+                    <Notifications sx={{ fontSize: "25px" }} />
+                    <Help sx={{ fontSize: "25px" }} />
+                    <FormControl variant="standard" value={fullName}>
+                        <Select
+                            value={fullName}
+                            sx={{
+                                backgroundColor: neutralLight,
+                                width: "150px",
+                                borderRadius: "0.25rem",
+                                p: ".25rem 1rem",
+                                "& .MuiSvgIcon-root": {
+                                    pr: ".25rem",
+                                    width: "3rem",
+                                },
+                                "& .MuiSelect-select:focus": {
+                                    backgroundColor: neutralLight,
+                                },
+                            }}
+                            input={<InputBase />}
+                        >
+                            <MenuItem value={fullName}>
+                                <Typography>{fullName}</Typography>
+                            </MenuItem>
+                            <MenuItem onClick={() => dispatch(setLogout)}>
+                                Log Out
+                            </MenuItem>
+                        </Select>
+                    </FormControl>
+                </FlexBetween>
+            ) : (
+                <IconButton
+                    onClick={() => {
+                        setIsMobileMenuToggled(!isMobileMenuToggled);
+                    }}
+                >
+                    <Menu />
+                </IconButton>
+            )}
         </FlexBetween>
     );
 };
